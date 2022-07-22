d9a5f9b665647015dc5fd40ef31ff00fd2497cbd
mathias <m.freiwald@gmx.net>
2022-07-22 18:16:08 +0200

    getting ready for deployment

--- home/utils.py
+++ home/utils.py
@@ -1,8 +1,6 @@
 def check_http_header_for_accept_language(request):
     '''
     Extract and process header-data from request.META to set the content language for the view.
-    This is self-written amateur code, Iam not sure if it is the right way to handle request.META.
-    Mathias Freiwald
     '''
 
     content_language = 'en'
@@ -18,12 +16,14 @@ def check_http_header_for_accept_language(request):
             if item[0].lower() == 'de' or item[0].lower() == 'de-de' or item[0].lower() == 'de-li' or\
                     item[0].lower() == 'de-lu' or item[0].lower() == 'de-at' or item[0].lower() == 'de-ch':
                 content_language = 'de'
+
         '''
         print('HTTP_ACCEPT_LANGUAGE: ', request.META['HTTP_ACCEPT_LANGUAGE'])
         print(first_split)
         print(final_split)
         print(content_language)
         '''
+
         return content_language
     else:
         return content_language
