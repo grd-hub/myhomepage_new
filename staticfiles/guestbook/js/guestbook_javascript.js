40438d48c887fce8680a776c5710468cc2c64389
mathias <m.freiwald@gmx.net>
2022-07-23 20:04:27 +0200

    ready for deplay?

---
+++ staticfiles/guestbook/js/guestbook_javascript.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/* --- START jQuery / UI --- */
+$(":radio").checkboxradio();
+$("#filter_button").button();
+/* --- END jQuery / UI ---*/
+
+
+/* --- START collapsible JS --- */
+const coll = document.getElementsByClassName("collapsible");
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+/* --- END collapsible JS --- */
+
+
+/* --- START TOGGLE MENU BUTTON jQuery --- */
+$("#toggle_menu_btn").click(function(){
+    $(".main_menu").toggle();
+});
+/* --- END TOGGLE MENU jQuery --- */
